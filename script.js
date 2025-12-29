04e609a4f5832389db9bd436c1cfa962ef8c1974
Rahul-2415 <rahulsingh8171335162@gmail.com>
2025-12-29 15:46:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
 //your JS code here. If required.
+
+
+const fontSizeInput = document.getElementById("fontsize");
+const fontColorInput = document.getElementById("fontcolor");
+const form = document.querySelector("form");
+
+
+function setCookie(name, value, days = 365) {
+  const expires = new Date();
+  expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+  document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
+}
+
+
+function getCookie(name) {
+  const cookies = document.cookie.split("; ");
+  for (let cookie of cookies) {
+    const [key, value] = cookie.split("=");
+    if (key === name) return value;
+  }
+  return null;
+}
+
+
+function applyPreferences() {
+  const savedFontSize = getCookie("fontsize");
+  const savedFontColor = getCookie("fontcolor");
+
+  if (savedFontSize) {
+    document.documentElement.style.setProperty(
+      "--fontsize",
+      `${savedFontSize}px`
+    );
+    fontSizeInput.value = savedFontSize;
+  }
+
+  if (savedFontColor) {
+    document.documentElement.style.setProperty(
+      "--fontcolor",
+      savedFontColor
+    );
+    fontColorInput.value = savedFontColor;
+  }
+}
+
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const fontSize = fontSizeInput.value;
+  const fontColor = fontColorInput.value;
+
+  setCookie("fontsize", fontSize);
+  setCookie("fontcolor", fontColor);
+
+  document.documentElement.style.setProperty("--fontsize", `${fontSize}px`);
+  document.documentElement.style.setProperty("--fontcolor", fontColor);
+});
+
+
+applyPreferences();
